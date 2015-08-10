e2f0686d2a0ee972cd09297b07d50650f8939371
James Baskerville <baskerville@college.harvard.edu>
2015-08-10 16:01:34 -0700

    hello

---
+++ dist/scripts.js
@@ -0,0 +1,39 @@
+/*! hmcvb.github.io - v1.0.0 - 2015-08-10
+* https://github.com/hmcvb/hmcvb.github.io#readme
+* Copyright (c) 2015 James Baskerville; Licensed under ISC 
+* Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+* THE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.*/
+
+$('body').scrollspy({
+  target: '#navbar-collapsible',
+  offset: 52
+});
+
+/* smooth scrolling sections */
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 50
+        }, 800);
+        
+        if (this.hash=="#section1") {
+            $('.scroll-up').hide();
+        }
+        else {
+            $('.scroll-up').show();
+        }
+        
+        
+        // activte animations in this section
+        target.find('.animate').delay(1200).addClass("animated");
+        setTimeout(function(){
+            target.find('.animated').removeClass("animated");
+        },2000);
+        
+        return false;
+      }
+    }
+});
