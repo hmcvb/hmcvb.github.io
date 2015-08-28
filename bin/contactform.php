58f82ff34588462d914c619458f404865a151c43
James Baskerville <baskerville@college.harvard.edu>
2015-08-28 13:27:03 -0700

    changed php and form handling

--- bin/contactform.php
+++ bin/contactform.php
@@ -1,16 +1,55 @@
-<?php 
-
-$action=$_REQUEST['action']; 
-
-if($action=="POST")
+<?php
+if($_POST)
 {
-    /* send the submitted data */ 
-
-    $name=$_REQUEST['name']; 
-    $email=$_REQUEST['email']; 
-    $message=$_REQUEST['message'];
-    $from="From: <$email>"; 
-    $subject="HMCVB: Message from $name"; 
-    mail("hmcvb@hcs.harvard.edu", $subject, "From: $name <$email>: \n \n $message", $from);
+    $to_email = "hmcvb@hcs.harvard.edu";
+    
+    //check if its an ajax request, exit if not
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+        
+        $output = json_encode(array( //create JSON data
+            'type'=>'error', 
+            'text' => 'Sorry, request must be Ajax POST'
+        ));
+        die($output); //exit script outputting json data
+    } 
+    
+    //Sanitize input data using PHP filter_var().
+    $user_name      = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
+    $user_email     = filter_var($_POST["user_email"], FILTER_SANITIZE_EMAIL);
+    $message        = filter_var($_POST["msg"], FILTER_SANITIZE_STRING);
+    
+    //additional php validation
+    if(strlen($user_name)<4){ // If length is less than 4 it will output JSON error.
+        $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
+        die($output);
+    }
+    if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)){ //email validation
+        $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
+        die($output);
+    }
+    if(strlen($message)<3){ //check emtpy message
+        $output = json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
+        die($output);
+    }
+    
+    //email body
+    $message_body = $message."\r\n\r\n-".$user_name."\r\nEmail : ".$user_email
+    
+    //proceed with PHP email.
+    $headers = 'From: '.$user_name.'' . "\r\n" .
+    'Reply-To: '.$user_email.'' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    
+    $send_mail = mail($to_email, $subject, $message_body, $headers);
+    
+    if(!$send_mail)
+    {
+        //If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
+        $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please email hmcvb@hcs.harvard.edu manually!'));
+        die($output);
+    }else{
+        $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$user_name .'. Thank you for your email!'));
+        die($output);
+    }
 }
-?> 
+?>
--- bin/mailinglist.php
+++ bin/mailinglist.php
@@ -1,15 +1,52 @@
-<?php 
-
-$action=$_REQUEST['action']; 
-
-if($action=="POST")
+<?php
+if($_POST)
 {
-    /* send the submitted data */ 
+    $to_email = "hmcvb@hcs.harvard.edu";
+    
+    //check if its an ajax request, exit if not
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+        
+        $output = json_encode(array( //create JSON data
+            'type'=>'error', 
+            'text' => 'Sorry Request must be Ajax POST'
+        ));
+        die($output); //exit script outputting json data
+    } 
+    
+    //Sanitize input data using PHP filter_var().
+    $user_name      = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
+    $user_email     = filter_var($_POST["user_email"], FILTER_SANITIZE_EMAIL);
+    $message        = filter_var($_POST["msg"], FILTER_SANITIZE_STRING);
+    
+    //additional php validation
+    if(strlen($user_name)<4){ // If length is less than 4 it will output JSON error.
+        $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
+        die($output);
+    }
+    if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)){ //email validation
+        $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
+        die($output);
+    }
 
-    $name=$_REQUEST['name']; 
-    $email=$_REQUEST['email']; 
-    $from="From: <$email>"; 
-    $subject="HMCVB: Add $name to Mailing List"; 
-    mail("hmcvb@hcs.harvard.edu", $subject, "$name <$email> would like to be added to the hmcvb mailing list", $from);
+    //email body
+    $subject = 'HMCVB: Add to Mailing List'
+    $message_body = 'Add'.$user_name.'<'.$user_email.'> to the mailing list.'
+    
+    //proceed with PHP email.
+    $headers = 'From: '.$user_name.'' . "\r\n" .
+    'Reply-To: '.$user_email.'' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    
+    $send_mail = mail($to_email, $subject, $message_body, $headers);
+    
+    if(!$send_mail)
+    {
+        //If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
+        $output = json_encode(array('type'=>'error', 'text' => 'Something didn\'t work! Please email hmcvb@hcs.harvard.edu manually to be added!'));
+        die($output);
+    }else{
+        $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$user_name .' Thank you for signing up for our mailing list. You\'ll be added soon!'));
+        die($output);
+    }
 }
-?> 
+?>
